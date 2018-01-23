daf7aadf71f7b80d8eb8fe23d8e9c9217908995e
Stefano Pernat <stefano.pernat@gmail.com>
2018-01-23 18:36:26 +0100

    Variables and Constants

---
+++ Introduction/variable_constants.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+/*
+ let name = "John Doe"
+ name = "Romeo"         // Error cannot re-assign a constant
+ */
+
+var name = "John Doe"   // Type Inference
+name = "Romeo"
+
+var age: Int
+age = 23
+
+var latitude: Double
+latitude = 36.166667
+
+//var longitude: Float      // Less accuracy then Double
+var longitude: Double
+longitude = -86.783333
+
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+var sisterName: String = "Jane"
