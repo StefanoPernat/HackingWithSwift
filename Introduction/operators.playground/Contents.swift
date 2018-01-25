03d09949f81aa6d457fb2722a11ea4b5a1aaec0a
Stefano Pernat <stefano.pernat@gmail.com>
2018-01-25 21:26:14 +0100

    Operators

---
+++ Introduction/operators.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var a = 10
+
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var a1 = 1.1
+var b1 = 2.2
+var c = a1 + b1
+
+var name1 = "Jane Doe"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+c > 3
+c >= 3
+c > 4
+c < 4
+
+name1 == "Jane Doe"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+name1 != "Romeo"
