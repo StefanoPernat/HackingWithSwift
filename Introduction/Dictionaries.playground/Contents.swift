f1c3af3898f54e5a22e02cc18cfbef252ca62199
Stefano Pernat <stefano.pernat@gmail.com>
2018-02-04 19:32:14 +0100

    Dictionaries

---
+++ Introduction/Dictionaries.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var person = [
+    "first"     : "Elisabetta",
+    "last"      : "Lemme",
+    "month"     : "April",
+    "website"   : "https://www.bettalemme.com/"
+]
+
+person["first"]
+person["month"]
+
+// creating dictionaries
+var another_person = [String: String]()
+var another_person2: [String: String] = [:]
+var another_person3 = Dictionary<String, String>()
