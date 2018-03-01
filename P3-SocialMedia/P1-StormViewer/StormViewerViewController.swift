657766fa24b192e92610fb6df5cc108d6a330df3
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-01 20:47:08 +0100

    Copy Project 1 as base code for project 3

---
+++ P3-SocialMedia/P1-StormViewer/StormViewerViewController.swift
@@ -0,0 +1,61 @@
+//
+//  StormViewerViewController.swift
+//  P1-StormViewer
+//
+//  Created by Stefano Pernat on 24/02/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class StormViewerViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+//                print(item)
+                pictures.append(item)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // - MARK: TableView callbacks
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
