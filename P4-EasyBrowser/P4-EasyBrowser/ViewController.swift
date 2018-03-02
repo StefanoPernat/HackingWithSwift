d6c2b0db73f197836dfa22ef5ef47f838097621b
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-02 20:30:50 +0100

    Add template code

---
+++ P4-EasyBrowser/P4-EasyBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P4-EasyBrowser
+//
+//  Created by Stefano Pernat on 02/03/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
