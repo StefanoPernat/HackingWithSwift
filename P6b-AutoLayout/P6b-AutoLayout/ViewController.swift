669e1d1f100a3d39c611fee8ac5549d0409bb6c2
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-15 20:48:09 +0100

    Add template code for project 6b

---
+++ P6b-AutoLayout/P6b-AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P6b-AutoLayout
+//
+//  Created by Stefano Pernat on 15/03/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
