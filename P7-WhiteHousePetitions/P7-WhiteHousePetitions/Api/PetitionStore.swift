eefa16d215b979e51c573fced8d327827fa32417
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-19 22:03:47 +0100

    Fix fetching methods

--- P7-WhiteHousePetitions/P7-WhiteHousePetitions/Api/PetitionStore.swift
+++ P7-WhiteHousePetitions/P7-WhiteHousePetitions/Api/PetitionStore.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class PetitionStore {
+    // - MARK: Properties
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -16,9 +17,14 @@ class PetitionStore {
     
     var petitions: [Petition]?
     
+    // - MARK: Public Methods
     public func fetchMostRecent(limitedTo limit: Int) {
         let url = PetitionsAPI.buildURL(forMethod: .mostRecent, limitedTo: limit)
-        print(url.absoluteString)
+        fetch(fromURL: url, limitedTo: limit)
+    }
+    
+    // - MARK: Private Methods
+    private func fetch(fromURL url: URL, limitedTo limit: Int) {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             [unowned self] (data, response, error) in
@@ -29,19 +35,14 @@ class PetitionStore {
                     let jsonDictionary = jsonObject as? [AnyHashable: Any],
                     let jsonArray = jsonDictionary["results"] as? [[String: Any]]
                 else {
-                    self.petitions = nil
-                    return
+                        self.petitions = nil
+                        return
                 }
                 
                 self.petitions = self.parse(fromJSON: jsonArray)
-                print(self.petitions!.count)
             }
         }
         task.resume()
-    }
-    
-    private func fetch(fromURL url: URL, limitedTo limit: Int) {
-        
     }
     
     private func parse(fromJSON json: [[String: Any]]) -> [Petition]? {
