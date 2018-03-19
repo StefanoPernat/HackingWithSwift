f740ab49ccbc0f76e03505d8d08b222859a6af55
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-19 22:02:54 +0100

    Add comments into PetitionsAPI

--- P7-WhiteHousePetitions/P7-WhiteHousePetitions/Api/PetitionsAPI.swift
+++ P7-WhiteHousePetitions/P7-WhiteHousePetitions/Api/PetitionsAPI.swift
@@ -20,10 +20,10 @@ enum Method {
 
 
 struct PetitionsAPI {
-    
-    
+    // - MARK: Properties
     private static let baseURLString = "https://api.whitehouse.gov/v1/petitions.json"
     
+    // - MARK: Methods
     public static func buildURL(forMethod method: Method, limitedTo limit: Int) -> URL {
         // divide url into components
         var components = URLComponents(string: baseURLString)!
@@ -43,48 +43,4 @@ struct PetitionsAPI {
         
         return components.url!
     }
-    
-    /*public static func fetchMostRecent(limit: Int) -> [Petition]? {
-        var petitionsArray: [Petition]? = nil
-        let url = buildURL(forMethod: .mostRecent, limitedTo: limit)
-        let request = URLRequest(url: url)
-        let task = session.dataTask(with: request) {
-            (data, response, error) in
-            
-            if error == nil {
-                print("error is not nil")
-                let jsonObject = try! JSONSerialization.jsonObject(with: data!, options: [])
-                print(jsonObject)
-            }
-            
-        }
-        task.resume()
-        return nil
-    }
-    
-    public static func fetchMostSigned(threshold: Int, limit: Int) -> [String]? {
-        let url = buildURL(forMethod: .mostSigned(signatureThreshold: threshold), limitedTo: limit)
-        
-        print(url.absoluteString)
-        
-        return nil
-    }
-    
-    private static func parseJSON(data: Data) -> [Petition]? {
-        do {
-            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-            guard
-            let jsonDictionary = jsonObject as? [AnyHashable: Any],
-            let jsonResult = jsonDictionary["result"] as? [Int: Any]
-            else {
-                return nil
-            }
-            
-            print(jsonObject)
-            return nil
-        } catch  {
-            return nil
-        }
-    }*/
-    
 }
