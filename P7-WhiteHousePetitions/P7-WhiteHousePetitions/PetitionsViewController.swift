45a3e0a45563735d095e2e1e1b6ea5382a56d688
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-20 21:11:48 +0100

    Implement tableview methods

--- P7-WhiteHousePetitions/P7-WhiteHousePetitions/PetitionsViewController.swift
+++ P7-WhiteHousePetitions/P7-WhiteHousePetitions/PetitionsViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class PetitionsViewController: UITableViewController {
 
+    var petitionsStore = PetitionsStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        petitionsStore.fetchMostRecent(limitedTo: 100) {
+            [unowned self] (petitionResult) -> Void in
+            
+            if case .success(let petitions) = petitionResult {
+                self.petitionsStore.petitions = petitions
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
         
-        PetitionsAPI.fetchMostRecent(limit: 100)
-        PetitionsAPI.fetchMostSigned(threshold: 10_000, limit: 100)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.petitionsStore.petitions?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Petition", for: indexPath)
+        cell.textLabel?.text = "Petition Title"
+        cell.detailTextLabel?.text = "Petition Body"
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
