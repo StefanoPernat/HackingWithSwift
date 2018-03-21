4fc9af78ceda3832c5629276ef3fe8fa4595abf9
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-21 10:35:34 +0100

    Add PetitionDetailViewController

---
+++ P7-WhiteHousePetitions/P7-WhiteHousePetitions/PetitionDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PetitionDetailViewController.swift
+//  P7-WhiteHousePetitions
+//
+//  Created by Stefano Pernat on 21/03/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class PetitionDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
