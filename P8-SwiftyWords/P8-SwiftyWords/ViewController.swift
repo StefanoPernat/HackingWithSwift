c098eeea56c9966c71d3dbd348c3cf8fc34f64ae
Stefano Pernat <stefano.pernat@gmail.com>
2018-03-27 19:14:21 +0200

    Add template code

---
+++ P8-SwiftyWords/P8-SwiftyWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P8-SwiftyWords
+//
+//  Created by Stefano Pernat on 27/03/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
