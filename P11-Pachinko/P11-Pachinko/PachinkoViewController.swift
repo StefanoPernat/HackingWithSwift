2de002897258720143d854659e7a09dc8db1a4ee
Stefano Pernat <stefano.pernat@gmail.com>
2018-04-07 12:19:41 +0200

    Rename standard view controller

--- P11-Pachinko/P11-Pachinko/PachinkoViewController.swift
+++ P11-Pachinko/P11-Pachinko/PachinkoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class PachinkoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
