893db0dc8b423933450036361f3d34f08c42ebaa
Stefano Pernat <stefano.pernat@gmail.com>
2018-04-11 19:30:18 +0200

    Add names to ball and slot base

--- P11-Pachinko/P11-Pachinko/GameScene.swift
+++ P11-Pachinko/P11-Pachinko/GameScene.swift
@@ -28,6 +28,8 @@ class GameScene: SKScene {
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
             ball.physicsBody?.restitution = 0.4
             ball.position = location
+            ball.name = "ball"
+            
             addChild(ball)
         }
         
@@ -62,9 +64,11 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         
         slotBase.position = position
