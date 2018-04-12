555ed0587949ef72ce6e7afe105bcd476ed53304
Stefano Pernat <stefano.pernat@gmail.com>
2018-04-12 20:21:15 +0200

    Update destroy with fire particles

--- P11-Pachinko/P11-Pachinko/GameScene.swift
+++ P11-Pachinko/P11-Pachinko/GameScene.swift
@@ -152,6 +152,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func destroy(_ ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
         ball.removeFromParent()
     }
     
