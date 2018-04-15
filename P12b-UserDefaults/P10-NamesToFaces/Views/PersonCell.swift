fab5ba9c3bb867b8a48e92b2e394e72f04e43bc2
Stefano Pernat <stefano.pernat@gmail.com>
2018-04-15 11:14:01 +0200

    Add starting code for second part

---
+++ P12b-UserDefaults/P10-NamesToFaces/Views/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  P10-NamesToFaces
+//
+//  Created by Stefano Pernat on 03/04/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var picture: UIImageView!
+    @IBOutlet var name: UILabel!
+}
