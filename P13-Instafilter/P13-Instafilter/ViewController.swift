42c3c73b45632a943c59a2594edf6b72ed3d8d6b
Stefano Pernat <stefano.pernat@gmail.com>
2018-04-18 17:53:45 +0200

    Add template code

---
+++ P13-Instafilter/P13-Instafilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P13-Instafilter
+//
+//  Created by Stefano Pernat on 18/04/18.
+//  Copyright © 2018 Stefano Pernat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
