b0ff44efee5b71f09dc81b42acae132a45060eaa
Stefano Pernat <stefano.pernat@gmail.com>
2018-06-08 12:21:03 +0200

    Implement tapped action

--- P15-Animations/P15-Animations/AnimationsViewController.swift
+++ P15-Animations/P15-Animations/AnimationsViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class AnimationsViewController: UIViewController {
 
     // MARK: - Properties
+    var imageView: UIImageView!
+    var currentAnimation = 0
     
     // MARK: - Outlets
     @IBOutlet weak var tap: UIButton!
@@ -19,6 +21,10 @@ class AnimationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 184)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +34,29 @@ class AnimationsViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func tapped(_ sender: UIButton) {
+        tap.isHidden = true
         
+        UIView.animate(
+            withDuration: 1,
+            delay: 0,
+            options: [],
+            animations: { [unowned self] in
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        },
+            completion: { [unowned self] (finished: Bool) in
+                self.tap.isHidden = false
+            })
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
     
 }
